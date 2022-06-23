5178b6aee9b1297f92da84ebbb0995b79d710024
suneel3214 <suneelchandel935@gmail.com>
2022-06-23 03:08:37 -0700

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,65 +19,63 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-            <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel" style="float:right;">
-            <div class="container">
-                <!-- <a class="navbar-brand" href="#">
-               OBC ALL INDIA
-                </a> -->
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-    
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto"></ul>
+                <div id="app">
+                    <nav class="navbar navbar-expand-md navbar-light navbar-laravel" style="float:right;">
+                        <div class="container">
+                            <!-- <a class="navbar-brand" href="#">
+                           OBC ALL INDIA
+                            </a> -->
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <!-- Left Side Of Navbar -->
+                                <ul class="navbar-nav mr-auto"></ul>
 
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        <!-- @guest -->
-                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        <!-- @else -->
-                            <!-- <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
-                            <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
-                            <li><a class="nav-link" href="{{ route('products.index') }}">Manage Product</a></li> -->
-                            <!-- <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                                <!-- Right Side Of Navbar -->
+                                <ul class="navbar-nav ml-auto">
+                                    <!-- Authentication Links -->
+                                    <!-- @guest -->
+                                        <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                                        <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                                    <!-- @else -->
+                                        <!-- <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
+                                        <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
+                                        <li><a class="nav-link" href="{{ route('products.index') }}">Manage Product</a></li> -->
+                                        <!-- <li class="nav-item dropdown">
+                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                {{ Auth::user()->name }} <span class="caret"></span>
+                                            </a>
 
 
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                                   onclick="event.preventDefault();
+                                                                 document.getElementById('logout-form').submit();">
+                                                    {{ __('Logout') }}
+                                                </a>
 
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li> -->
-                        <!-- @endguest -->
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-
-        <main class="py-4">
-            <div class="container">
-                <div class="app-content">
-                     @yield('content')
-                </div>     
-            </div>
-        </main>
-    </div>
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                    @csrf
+                                                </form>
+                                            </div>
+                                        </li> -->
+                                    <!-- @endguest -->
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                    <main class="py-4">
+                        <div class="container">
+                            <div class="app-content">
+                                 @yield('content')
+                            </div>     
+                        </div>
+                    </main>
+                 </div>
             </div>
         </div>
     </div>
